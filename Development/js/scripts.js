40dff05151b01dc4f540a083efb80945a6c7cb4c
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-07-01 21:19:50 +0300

    Finished half one
    Sorry for didn't continue all the work

---
+++ Development/js/scripts.js
@@ -0,0 +1,27 @@
+// var firstOne = ["Menawer", "Moudhi", "Mohammed", "Hussain", "Rahaf", "Hussain"];
+
+// function logger() {
+//   console.log(firstOne);
+// }
+
+// logger();
+
+//----------------------------------------------------------------------------
+
+// var Temperature = [30, 35, 40, 45, 50, 52];
+
+// var Temperature2 = [];
+
+// var i;
+
+// function hottestDays() {
+//   for (i = 0; i < Temperature.length; i++) {
+//     if (Temperature[i] > 40) {
+//       Temperature2.push(Temperature[i]);
+//     }
+//   }
+//   console.log(Temperature2);
+// }
+// hottestDays();
+
+//----------------------------------------------------------------------------
