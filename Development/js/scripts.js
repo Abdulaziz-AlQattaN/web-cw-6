bc54d348849e6c707c7b36c7383b7679a5206e80
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-07-02 17:28:32 +0300

    Little to done!
    Little to done!

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -25,3 +25,54 @@
 // hottestDays();
 
 //----------------------------------------------------------------------------
+// let bybooks = [
+//   {
+//     bookid: 0,
+//     bookname: "poor dad rich dad",
+//   },
+//   {
+//     bookid: 1,
+//     bookname: "Launch",
+//   },
+//   {
+//     bookid: 2,
+//     bookname: "14000 Things To be Happy about",
+//   },
+// ];
+
+// function getBookById() {
+//   let i;
+//   let pro = parseInt(prompt("Choose Your book id"));
+//   let Foundbook = false;
+
+//   for (i = 0; i < bybooks.length; i++) {
+//     if (pro == bybooks[i].bookid) {
+//       alert("Your book is avaliable");
+//       Foundbook = true;
+//       break;
+//     }
+//   }
+//   if (Foundbook == false) {
+//     alert("Your book isn't avaliable");
+//   }
+// }
+
+let product = prompt("السلعة: ");
+let price = parseInt(prompt("السعر"));
+let quantity = parseInt(prompt("الكمية"));
+
+let products = [];
+let prices = [];
+let quantities = [];
+
+while (product != "done") {
+  products.push(product);
+  prices.push(price);
+  quantities.push(quantity);
+
+  if (product != "انتهيت") {
+    product = prompt("السلعة: ");
+    price = parseInt(prompt("السعر"));
+    quantity = parseInt(prompt("الكمية"));
+  }
+}
